49339be2c598c078e3540bb9517eea63285773b4
killergot <disney.132435morozko@mail.ru>
2025-04-11 23:14:38 +0300

    del trash main.sql

--- app/main.sql
+++
@@ -1,9 +0,0 @@
--- 1. Таблица пользователей (users)
-CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    full_name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
